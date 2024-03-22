9bcd4b7206f950b26a12638cfda524fe076d7681
ddiegoxx <dm1592200@gmail.com>
2024-03-22 20:28:45 -0300

    adicionando disciplina e curso

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Aluno
+from .models import Aluno, Curso, Disciplina
 
 admin.site.register(Aluno)
+admin.site.register(Curso)
+admin.site.register(Disciplina)
--- alunos/models.py
+++ alunos/models.py
@@ -4,4 +4,23 @@ from django.db import models
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=200)
-    data_nascimento = models.DateTimeField("date published")
+    data_nascimento = models.DateField("date published")
+    
+    def  __str__(self):
+        return self.nome
+    
+    
+class Curso(models.Model):
+    nome = models.CharField(max_length=200)
+    
+    def  __str__(self):
+        return self.nome
+    
+    
+class Disciplina(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=6, null = True)
+    
+    def  __str__(self):
+        return self.nome
